92e96f86217459ef919c99e298d7e6ed9f9dbe71
mitsuaki1987 <kawamitsuaki@gmail.com>
2016-06-15 16:32:03 +0900

    Backup

--- src/shifted_krylov.f90
+++ src/shifted_krylov.f90
@@ -1,4 +1,4 @@
-MODULE syfted_krylov
+MODULE shifted_krylov_vals
   !
   IMPLICIT NONE
   !
@@ -21,48 +21,117 @@ MODULE syfted_krylov
   & v3(:), &
   & pi(:), &
   & pi_old(:), &
+  & pi_save(:), &
   & p(:,:), &
   & v3(:), &
   & alpha_save(:), &
   & beta_save(:), &
-  & r_small_save(:,:)
+  & r_l_save(:,:)
+  !
+END MODULE shifted_krylov_vals
+!
+! Routines for real-valiable CG
+!
+MODULE shifted_krylov
   !
 CONTAINS
 !
-SUBROUTINE CG_R_restart(ndim0, nl0, nz0, v2, x, z0, itermax0, &
-&                       threshold0, r, r_old, &
-&                       alpha_save0, beta_save0, zseed0, r_small_save0)
-  !
-  USE mathlib, ONLY : dcopy
-  !
-  IMPLICIT NONE
-  !
-  INTEGER,INTENT(IN) :: ndim0, nl0, nz0
-  REAL(8),INTENT(IN) :: z0(nz0)
-  REAL(8),INTENT(OUT) :: x(nl0,nz0)
-  INTEGER,INTENT(IN) :: itermax0, threshold0
-  !
-  ! For Restarting
-  !
-  REAL(8),INTENT(IN) :: &
-  & r(ndim), r_old(ndim), alpha_save0(iter_old), beta_save0(iter_old), &
-  & zseed0, r_small_save0(nl,iter_old)
-  !
-  CALL CG_R_init(ndim0, nl0, nz0, x, z0, itermax0, threshold0)
-
-
-END SUBROUTINE CG_R_restart
+! Shifted Part
 !
-SUBROUTINE CG_R_init(ndim0, nl0, nz0, x, z0, itermax0, threshold0)
+SUBROUTINE CG_R_shiftedeqn(r_l, x)
   !
+  USE shifted_krylov_vals, ONLY : alpha, beta, itermax, nl, nz, &
+  &                               p, pi, pi_old, pi_save, z, zseed
+  USE mathlib, ONLY : daxpy
+  !
+  IMPLICIT NONE
+  !
+  REAL(8),INTENT(IN) :: r_l(nl)
+  REAL(8),INTENT(INOUT) :: x(nl,nz)
+  !
+  INTEGER :: iz
+  REAL(8) :: pi_new
+  !
+  DO iz = 1, nz
+     !
+     pi_new = (1d0 + alpha * (z(iz) - zseed)) * pi(iz) &
+     &      - alpha * beta / alpha_old * (pi_old(iz) - pi(iz))
+     p(1:nl,iz) = r_l(1:nl) / pi(iz) &
+     &          + (pi_old(iz) / pi(iz))**2 * beta * p(1:nl,iz)
+     CALL daxpy(ndim,pi(iz)/ pi_old(iz) * alpha,p(1:nl,iz),1,x(1:nl,iz),1)
+     pi_old(iz) = pi(iz)
+     pi(iz) = pi_new
+     !
+     IF(itermax > 0) pi_save(iz,iter) = pi_new
+     !
+  END DO
+  !
+END SUBROUTINE CG_R_shiftedeqn
+!
+! Seed Switching
+!
+SUBROUTINE CG_R_seed_switch(v2)
+  !
+  USE mathlib, ONLY : dscal
+  USE shifted_krylov_vals, ONLY : alpha, alpha_save, beta_save, &
+  &                               iter, itermax, ndim, nz, pi, pi_old, rho, &
+  &                               v3, zseed
+  !
+  IMPLICIT NONE
+  !
+  REAL(8),INTENT(INOUT) :: v3(ndim)
+  !
+  INTEGER :: izseed
+  REAL(8) :: scale
+  !
+  izseed = MINLOC(pi(1:nz), 1)
+  !
+  IF(ABS(zseed - z(izseed)) > 1d-12) THEN
+     !
+     zseed = z(izseed)
+     !
+     alpha = alpha * pi_old(izseed) / pi(izseed)
+     rho = rho / pi_old(izseed)
+     !
+     scale = 1d0 / pi(izseed)
+     CALL dscal(ndim,scale,v2,1)
+     CALL dscal(nz,scale,pi,1)
+     scale = 1d0 / pi_old(izseed)
+     CALL dscal(ndim,scale,v3,1)
+     CALL dscal(nz,scale,pi_old,1)
+     !
+     ! For restarting
+     !
+     IF(itermax > 0) THEN
+        !
+        do jter = 1, iter
+           alpha_save(jter) = alpha_save(jter) &
+           &                * pi_save(izseed, jter - 1) / pi_save(izseed,jter) 
+           beta_save(jter) = beta_save(jter) &
+           &               * (pi_save(izseed, jter - 2) / pi_save(izseed,jter - 1))**2 
+           scale = 1d0 / pi_save(izseed, jter)
+           CALL dscal(nz,scale,pi_save(1:nz,jter),1)
+        end do
+        !
+     END IF
+     !
+  END IF
+  !
+END SUBROUTINE CG_R_seed_switch
+!
+! Allocate & initialize variables
+!
+SUBROUTINE CG_R_init(ndim0, nl0, nz0, v2, x, z0, itermax0, threshold0, lconverged)
+  !
+  USE shifted_krylov, ONLY : alpha, alpha_save, beta, beta_save, iter, itermax, &
+  &                          ndim, nl, nz, p, pi, pi_old, pi_save, rho, r_l_save, v3, z, zseed 
   USE mathlib, ONLY : dcopy
   !
   IMPLICIT NONE
   !
-  INTEGER,INTENT(IN) :: ndim0, nl0, nz0
-  REAL(8),INTENT(IN) :: z0(nz0)
+  INTEGER,INTENT(IN) :: ndim0, nl0, nz0, itermax0
+  REAL(8),INTENT(IN) :: z0(nz0), threshold0
   REAL(8),INTENT(OUT) :: x(nl0,nz0)
-  INTEGER,INTENT(IN) :: itermax0, threshold0
   !
   ndim = ndim0
   nl = nl0
@@ -70,13 +139,13 @@ SUBROUTINE CG_R_init(ndim0, nl0, nz0, x, z0, itermax0, threshold0)
   itermax = itermax0
   threshold = threshold0
   !
-  ALLOCATE(z(nz), v3(ndim), pi(nz), piold(nz), p(nl,nz))
+  ALLOCATE(z(nz), v3(ndim), pi(nz), pi_old(nz), p(nl,nz))
   CALL dcopy(nz,z0,1,z,1)
   v3(1:ndim) = 0d0
   p(1:nl,1:nz) = 0d0
   x(1:nl,1:nz) = 0d0
   pi(1:nz) = 1d0
-  piold(1:nz) = 1d0
+  pi_old(1:nz) = 1d0
   rho = 1d0
   alpha = 1d0
   beta = 0d0
@@ -85,20 +154,103 @@ SUBROUTINE CG_R_init(ndim0, nl0, nz0, x, z0, itermax0, threshold0)
   !
   IF(itermax > 0) THEN
      ALLOCATE(alpha_save(itermax), beta_save(itermax), &
-     &        r_small_save(1:nl,itermax))
+     &        r_l_save(nl,itermax), pi_save(nz,-1:itermax))
+     pi_save(1:nz,-1:0) = 1d0
+  END IF
+  !
+  ! Convergence check
+  !
+  res = ddot(ndim,v2,1,v2,1)
+  !
+  IF(res < threshold) THEN
+     lconverged = 1
+  ELSE
+     lconverged = 0
   END IF
   !
 END SUBROUTINE CG_R_init
 !
-SUBROUTINE CG_R_update(v12, v2, x, r_small, lconverged)
+! Restart by input
+!
+SUBROUTINE CG_R_restart(ndim0, nl0, nz0, v2, x, z0, itermax0, threshold0, lconverged, &
+&                       iter_old, v12, alpha_save0, beta_save0, zseed0, r_l_save0)
+  !
+  USE shifted_krylov_vals, ONLY : alpha, alpha_old, alpha_save, beta, beta_save, iter, iter_max, &
+  &                               ndim, nl, r_l_save, threshold, v3, zseed
+  USE mathlib, ONLY : dcopy, daxpy
+  !
+  IMPLICIT NONE
+  !
+  INTEGER,INTENT(IN) :: ndim0, nl0, nz0, itermax0
+  REAL(8),INTENT(IN) :: z0(nz0), threshold0
+  REAL(8),INTENT(OUT) :: x(nl0,nz0)
+  INTEGER,INTENT(OUT) :: lconverged
+  !
+  ! For Restarting
+  !
+  INTEGER(8),INTENT(IN) :: iter_old
+  REAL(8),INTENT(IN) :: &
+  & r(ndim), r_old(ndim), alpha_save0(iter_old), beta_save0(iter_old), &
+  & zseed0, r_l_save0(nl,iter_old)
+  !
+  CALL CG_R_init(ndim0, nl0, nz0, v2, x, z0, itermax0, threshold0, lconverged)
+  zseed = zseed0
+  !
+  DO iter = 1, iter_old
+     !
+     beta = beta_save0(iter)
+     alpha_old = alpha
+     alpha = alpha_save0(iter)
+     !
+     ! For restarting
+     !
+     IF(itermax > 0) THEN
+        alpha_save(iter) = alpha
+        beta_save(iter) = beta
+        CALL dcopy(nl,r_l_save0(1:nl,iter),1,r_l_save(1:nl,iter),1)
+     END IF
+     !
+     ! Shifted equation
+     !
+     CALL CG_R_shiftedeqn(r_l_save0(1:nl,iter), x)
+     !
+  END DO
+  !
+  ! Rewind
+  !
+  iter = iter_old 
+  !
+  CALL dcopy(ndim,v12,1,v3,1)
+  !
+  ! Seed Switching
+  !
+  CALL CG_R_seed_switch(v2)
+  !
+  ! Convergence check
+  !
+  res = ddot(ndim,v2,1,v2,1)
+  !
+  IF(res < threshold) THEN
+     lconverged = iter
+  ELSE IF(iter == itermax)
+     lconverged = - iter
+  ELSE
+     lconverged = 0
+  END IF
+  !
+END SUBROUTINE CG_R_restart
+!
+! Update x, p, r
+!
+SUBROUTINE CG_R_update(v12, v2, x, r_l, lconverged)
   !
   USE mathlib, ONLY : daxpy, ddot, dcopy, dscale
   !
   IMPLICIT NONE
   !
   REAL(8),INTENT(INOUT) :: v12(ndim), v2(ndim), x(nl,nz)
-  REAL(8),INTENT(IN) :: r_small(nl)
-  LOGICAL,INTENT(OUT) :: lconverged
+  REAL(8),INTENT(IN) :: r_l(nl)
+  INTEGER,INTENT(OUT) :: lconverged
   !
   INTEGER :: iz, izseed
   REAL(8) :: rho_old, prod, alpha_old, scale, res
@@ -118,18 +270,12 @@ SUBROUTINE CG_R_update(v12, v2, x, r_small, lconverged)
   IF(itermax > 0) THEN
      alpha_save(iter) = alpha
      beta_save(iter) = beta
-     CALL dcopy(nl,r_small,1,r_small_save(1:nl,iter),1)
+     CALL dcopy(nl,r_l,1,r_l_save(1:nl,iter),1)
   END IF
   !
   ! Shifted equation
   !
-  DO iz = 1, nz
-     pi_new = (1d0 + alpha * (z(iz) - zseed)) * pi(iz) &
-     &      - alpha * beta / alpha_old * (pi_old(iz) - pi(iz))
-     p(1:nl,iz) = r_small_(1:nl) / pi(iz) &
-     &          + (pi_old(iz) / pi(iz))**2 * beta * p(1:nl,iz)
-     CALL daxpy(ndim,pi(iz)/ pi_old(iz) * alpha,p(1:nl,iz),1,x(1:nl,iz),1)
-  END DO
+  CALL CG_R_shiftedeqn(r_l, x)
   !
   ! Update residual
   !
@@ -141,44 +287,45 @@ SUBROUTINE CG_R_update(v12, v2, x, r_small, lconverged)
   !
   ! Seed Switching
   !
-  izseed = MINLOC(pi(1:nz), 1)
-  zseed = z(izseed)
-  scale = 1d0 / pi(izseed)
-  CALL dscal(nz,scale,pi,1)
-  CALL dscal(ndim,scale,v2,1)
-  scale = 1d0 / pi_old(izseed)
-  CALL dscal(nz,scale,pi_old,1)
-  CALL dscal(ndim,scale,v3,1)
+  CALL CG_R_seed_switch(v2)
   !
   ! Convergence check
   !
   res = ddot(ndim,v2,1,v12,1)
   IF(res < threshold) THEN
-     lconverged = .TRUE.
+     lconverged = iter
+  ELSE IF(iter == itermax)
+     lconverged = -iter
   ELSE
-     lconverged = .FALSE.
+     lconverged = 0
   END IF
   !
 END SUBROUTINE CG_R_update
 !
-SUBROUTINE CG_R_getcoef(alpha_save0, beta_save0, zseed0, r_small_save0)
+! Return saved alpha, beta, r_l
+!
+SUBROUTINE CG_R_getcoef(alpha_save0, beta_save0, zseed0, r_l_save0)
   !
+  USE shifted_krylov, ONLY : alpha_save, beta_save, r_l_save, iter, nl, zseed
   USE mathlib, ONLY : dcopy
   !
   IMPLICIT NONE
   !
   REAL(8),INTENT(OUT) :: alpha0(iter), beta(iter), zseed0, &
-  &                      r_small_save0(nl,iter)
+  &                      r_l_save0(nl,iter)
   !
   zseed0 = zseed
   CALL dcopy(iter,alpha_save,1,alpha_save0,1)
   CALL dcopy(iter,beta_save,1,beta_save0,1)
-  CALL dcopy(nl*iter,r_small_save,1,r_small_save0,1)
+  CALL dcopy(nl*iter,r_l_save,1,r_l_save0,1)
   !
 END SUBROUTINE CG_R_getcoef
 !
+! Return r_old
+!
 SUBROUTINE CG_R_getvec(r_old)
   !
+  USE shifted_krylof, ONLY : ndim, v3
   USE mathlib, ONLY : dcopy
   !
   IMPLICIT NONE
@@ -189,8 +336,20 @@ SUBROUTINE CG_R_getvec(r_old)
   !
 END SUBROUTINE CG_R_getvec
 !
+! Deallocate private arrays
+!
 SUBROUTINE CG_R_finalize()
-  DEALLOCATE(z, v3, pi, piold, p)
+  !
+  USE shifted_krylov, ONLY : alpha_save, beta_save, r_l_save, pi_save
+  !
+  IMPLICIT NONE
+  !
+  DEALLOCATE(z, v3, pi, pi_old, p)
+  !
+  IF(itermax > 0) THEN
+     DEALLOCATE(alpha_save, beta_save, r_l_save, pi_save)
+  END IF
+  !
 END SUBROUTINE CG_R_finalize
-
+!
 END MODULE syfted_krylov
--- src/solve_rr.f90
+++ src/solve_rr.f90
@@ -1,10 +1,4 @@
-PROGRAM solve_rr
-  !
-  USE shifted_krilov, ONLY : CG_R_init, CG_R_restart, CG_R_update, &
-  &                          CG_R_getcoef, CG_R_getvec, CG_R_finalize
-  USE my_module, ONLY : input_size, rhs_vector, input_restart, &
-  &                     projection, matrix_product, &
-  &                     output_restart, output_result
+MODULE solver_rr_vals
   !
   IMPLICIT NONE
   !
@@ -13,69 +7,183 @@ PROGRAM solve_rr
   & nz,      & ! Number of frequencies
   & nl,      & ! Number of Left vector
   & itermax, & ! Max. number of iteraction
-  & itermin, & ! First iteration in this run
-  & iter,    & ! Counter for Iteration
-  & niter_old  ! Number of iteraction of previous run
-  !
-  REAL(8) :: &
-  & threshold, & ! Convergence Threshold
+  & iter_old   ! Number of iteraction of previous run
   !
   REAL(8) :: &
   & v12(:), v2(:), & ! (ndim): Working vector
-  & rsmall(:), & ! (nl) : Projeccted residual vector 
+  & r_l(:), & ! (nl) : Projeccted residual vector 
   & x(:,:),    & ! (nl,nz) : Projected result 
   & z(:)         ! (nz): Frequency
   !
   ! Variables for Restart
   !
   REAL(8) :: &
-  & r(:), r_old(:), & ! (ndim) Residual vector in Current & Previous step
   & zseed, & ! Seed frequency
-  & alpha(:), beta(:), & ! (niter_old) 
-  & rsmall_save(:,:) ! (nl,niter_old) Projected residual vectors
+  & alpha(:), beta(:), & ! (iter_old) 
+  & r_l_save(:,:) ! (nl,iter_old) Projected residual vectors
   !
-  LOGICAL :: lconverged ! .TRUE. if converged
+END MODULE solver_rr_vals
+
+MODULE solver_rr_routines
+  !
+  IMPLICIT NONE
+  !
+CONTAINS
+  !
+SUBROUTINE input_size()
+  !
+  USE solver_rr_vals, ONLY : ndim, nz, itermax
+
+  IMPLICIT NONE
+  !
+  NAMELIST /input/ ndim, nz, itermax
+  !
+  read(*,input,err=100)
+  !
+  WRITE(*,*) "   Dimension of vvector : ", ndim
+  WRITE(*,*) "  Number of frequencies : ", nz
+  WRITE(*,*) "  Number of left vector : ", nl
+  WRITE(*,*) "       Max. iteractions : ", itermax
+  !
+  return
+  !
+100 write(*,*) "Stop in stdin. reading namelist file"
+  !
+  stop
+  !
+END SUBROUTINE input_size
+!
+! Input restart variables from file
+!
+SUBROUTINE input_restart()
+  !
+  USE solver_rr_vals, ONLY : iter_old, v2, v12, alpha, beta, zseed, r_l_save, nl, ndim
+  !
+  IMPLICIT NONE
+  !
+  INTEGER :: fi = 10, ierr
+  !
+  open(fi, file = 'restart.dat',status="old", action = 'read',iostat = ierr)
+  !
+  IF(ierr == 0) THEN
+     !
+     read(fi,*) iter_old
+     !
+     ALLOCATE(alpha(iter_old), beta(iter_old), r_l_save(nl,iter_old))
+     read(fi,*) zseed, v2(1:ndim), v12(1:ndim), &
+     &          alpha(1:iter_old), beta(1:iter_old), r_l_save(1:nl, 1:iter_old)
+     !
+     close(fi)
+     !
+  ELSE
+     !
+     iter_old = 0
+     !
+  END IF
+  !
+END SUBROUTINE input_restart
+!
+! Output variables for restart
+!
+SUBROUTINE output_restart()
+  !
+  USE solver_rr_vals, ONLY : iter_old, v2, v12, alpha, beta, zseed, r_l_save, nl, ndim
+  !
+  IMPLICIT NONE
+  !
+  INTEGER :: fo = 20
+  !
+  open(fo, file = 'restart.dat', action = 'write')
+  !
+  write(fo,*) iter_old
+  write(fo,*) zseed, v2(1:ndim), v12(1:ndim), &
+  &          alpha(1:iter_old), beta(1:iter_old), r_l_save(1:nl, 1:iter_old)
+  !
+END SUBROUTINE input_restart
+!
+! Check Result
+!
+SUBROUTINE output_result()
+  !
+  USE solver_rr_vals, ONLY : iter_old, v2, v12, alpha, beta, zseed, r_l_save, nl, ndim
+  !
+  IMPLICIT NONE
+  !
+  DO iz = 1, nz
+     !
+     v2(1:ndim) = z(iz) * x(1:ndim) - rhs(1:ndim)
+     CALL dgemv("N", ndim, ndim, -1d0, Ham, ndim, x(1:ndim,iz), 1, 1d0, v2, 1)
+     write(*,*) v2(1:ndim)
+     !
+  END DO
+  !
+END SUBROUTINE output_result
+!
+END MODULE solver_rr_routines
+!
+!
+!
+PROGRAM solve_rr
+  !
+  USE shifted_krilov, ONLY : CG_R_init, CG_R_restart, CG_R_update, &
+  &                          CG_R_getcoef, CG_R_getvec, CG_R_finalize
+  USE solver_rr_routines, ONLY : input_size, input_restart, &
+  &                              output_restart, output_result
+  !
+  IMPLICIT NONE
+  !
+  INTEGER :: &
+  & itermin, & ! First iteration in this run
+  & iter,    & ! Counter for Iteration
+  & lconverged
+  !
+  REAL(8) :: threshold ! Convergence Threshold
   !
   ! Input Size of vectors
   !
-  CALL input_size(ndim,nz,nl,itermax)
+  CALL input_size()
   !
-  ALLOCATE(v12(ndim), v2(ndim), rsmall(nl), x(nl,nz), z(nz))
-  !
-  ! Generate Right Hand Side Vector
-  !
-  CALL RANDOM_NUMBER(v2(1:ndim))
+  ALLOCATE(v12(ndim), v2(ndim), r_l(nl), x(nl,nz), z(nz), ham(ndim,ndim))
+  READ(*,*) z(1:nz)
+  CALL RANDOM_NUMBER(ham(1:ndim, 1:ndim))
+  CALL RANDOM_NUMBER(rhs(1:ndim))
   !
   ! Check: Whether the restart file is exist.
   !
-  CALL input_restart(niter_old, r, r_old, alpha, beta, zseed, rsmall_save)
+  CALL input_restart()
   !
-  IF(niter_old > 0) THEN
+  IF(iter_old > 0) THEN
     !
     ! When restarting, counter
     !
-    itermin = niter_old + 1
-    CALL CG_R_restart(ndim, nl, nz, v2, x, z, itermax, &
-    &                 threshold, r, r_old, &
-    &                 alpha, beta, zseed, rsmall_save)
+    itermin = iter_old + 1
+    CALL CG_R_restart(ndim, nl, nz, v2, x, z, itermax, threshold, lconverged, &
+    &                 iter_old, v12, alpha_save, beta_save, zseed, r_l_save)
     !
     ! These vectors were saved in CG_R routine
     !
-    DEALLOCATE(r, r_old, alpha, beta, zseed, rsmall_save)
+    DEALLOCATE(alpha, beta, r_l_save)
     !
   ELSE
-    itermin = 1
-    CALL CG_R_init(ndim, nl, nz, v2, x, z, itermax, threshold)
+     !
+     itermin = 1
+     !
+     ! Generate Right Hand Side Vector
+     !
+     v2(1:ndim) = rhs(1:ndim)
+     !
+     CALL CG_R_init(ndim, nl, nz, v2, x, z, itermax, threshold, lconverged)
+     !
   END IF
   !
   ! CG_R Loop
   !
-  DO iter = itermin, itermax
+  DO iter = 1, itermax - iter_old
      !
      ! Projection of Residual vector into the space
      ! spaned by left vectors
      !
-     rsmall(1:nl) = v2(1:ndim)
+     r_l(1:nl) = v2(1:ndim)
      !
      ! Matrix-vector product
      !
@@ -83,18 +191,25 @@ PROGRAM solve_rr
      !
      ! Update result x with CG_R
      !
-     CALL CG_R_update(v12, v2, x, rsmall, lconverged)
+     CALL CG_R_update(v12, v2, x, r_l, lconverged)
      !
-     IF(lconverged == .true.) EXIT
+     IF(lconverged /= 0) EXIT
      !
   END DO
   !
+  IF(lconverged < 0) THEN
+     WRITE(*,*) "  Not Converged in iteration ", -lconverged
+  ELSE
+     WRITE(*,*) "  Converged in iteration ", lconverged
+  END IF
+  iter_old = abs(lconverged)
+  !
   ! Get these vectors for restart in the Next run
   !
-  ALLOCATE(r(ndim), r_old(ndim), alpha(iter), beta(iter), rsmall_save(nl,iter))
+  ALLOCATE(alpha(iter_old), beta(iter_old), r_l_save(nl,iter_old))
   !
-  CALL CG_R_getcoef(alpha, beta, zseed, rsmall_save)
-  CALL CG_R_getvec(r, r_old)
+  CALL CG_R_getcoef(alpha_save, beta_save, zseed, r_l_save)
+  CALL CG_R_getvec(v12)
   !
   ! Deallocate all intrinsic vectors
   !
@@ -102,11 +217,11 @@ PROGRAM solve_rr
   !
   ! Output to a file
   !
-  CALL output_restart(niter_old, r, r_old, alpha, beta, zseed, rsmall_save)
+  CALL output_restart()
   !
-  CALL output_result(z,x)
+  CALL output_result()
   !
-  DEALLOCATE(r, r_old, alpha, beta, zseed, rsmall_save)
-  DEALLOCATE(v12, v2, rsmall, x, z)
+  DEALLOCATE(alpha, beta, zseed, r_l_save)
+  DEALLOCATE(v12, v2, r_l, x, z)
   !
 END PROGRAM solve_rr
