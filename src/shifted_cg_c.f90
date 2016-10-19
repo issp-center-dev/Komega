39347254b6eda7176822b8e7afac09b0abb275f4
mitsuaki1987 <kawamitsuaki@gmail.com>
2016-10-19 11:43:45 +0900

    Add Copylight

--- src/shifted_cg_c.f90
+++ src/shifted_cg_c.f90
@@ -1,4 +1,22 @@
 !
+!    Copyright 2016 Mitsuaki Kawamura
+!
+!    This file is part of ISSP Math Library.
+!
+!    ISSP Math Library is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU Lesser General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    ISSP Math Library is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU Lesser General Public License for more details.
+!
+!    You should have received a copy of the GNU Lesser General Public License
+!    along with ISSP Math Library.  If not, see <http://www.gnu.org/licenses/>.
+!
+!
 ! Routines for real-valiable CG
 !
 #if defined(MPI)
