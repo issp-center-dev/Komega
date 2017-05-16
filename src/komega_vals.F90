b6a8d983b71654b6fe01e51d091bc22c0e984db2
mitsuaki1987 <kawamitsuaki@gmail.com>
2017-05-16 12:54:57 +0900

    Modify for doxygen

--- src/komega_vals.F90
+++ src/komega_vals.F90
@@ -26,20 +26,20 @@ MODULE komega_parameter
   & almost0 = 1d-50
   !
   INTEGER,SAVE :: &
-  & comm, &
-  & iz_seed, &
-  & ndim, &
-  & nl,   &
-  & nz,   &
-  & itermax, &
-  & iter
+  & comm,    & !< Communicator for MPI
+  & iz_seed, & !< Index of frequency of seed
+  & ndim,    & !< Dimension of Hamiltonian
+  & nl,      & !< Dimension of projection
+  & nz,      & !< Number of Frequency (Shift)
+  & itermax, & !< Maximum number of iteration
+  & iter       !< Counter of iteration
   !
   REAL(8),SAVE :: &
-  & threshold, &
-  & resnorm
+  & threshold, & !< Convergence threshold
+  & resnorm      !< Residual norm
   !
   LOGICAL,ALLOCATABLE,SAVE :: &
-  & lz_conv(:)
+  & lz_conv(:) !< If converged at this frequency -> .TRUE.
   !
 #if defined(_KOMEGA_THREAD)
   !$OMP THREADPRIVATE(comm, iz_seed, ndim, nl, nz, itermax, &
@@ -48,26 +48,25 @@ MODULE komega_parameter
   !
 END MODULE komega_parameter
 !
-!
+!> Variables for CG
+!! \f$\alpha\f$
 !
 MODULE komega_vals_r
   !
   IMPLICIT NONE
   !
-  REAL(8),SAVE :: &
-  & z_seed, &
-  & rho, &
-  & alpha, &
-  & alpha_old, &
-  & beta
+  REAL(8),SAVE :: z_seed    !< Seed frequency
+  REAL(8),SAVE :: rho       !< \f$\rho\f$ of BiCG
+  REAL(8),SAVE :: alpha     !< \f$\alpha\f$ of BiCG
+  REAL(8),SAVE :: alpha_old !< \f$\alpha\f$ at the previous step
+  REAL(8),SAVE :: beta      !< \f$\beta\f$ of BiCG
   !
-  REAL(8),ALLOCATABLE,SAVE :: &
-  & z(:), &
-  & pi(:), &
-  & pi_old(:), &
-  & pi_save(:,:), &
-  & alpha_save(:), &
-  & beta_save(:)
+  REAL(8),ALLOCATABLE,SAVE :: z(:)          !< Frequencies
+  REAL(8),ALLOCATABLE,SAVE :: pi(:)         !< \f$\pi\f$ of BiCG
+  REAL(8),ALLOCATABLE,SAVE :: pi_old(:)     !< \f$\pi\f$ at the previous step
+  REAL(8),ALLOCATABLE,SAVE :: pi_save(:,:)  !< \f$\pi\f$ saved for the restart
+  REAL(8),ALLOCATABLE,SAVE :: alpha_save(:) !< \f$\alpha\f$ saved for restart
+  REAL(8),ALLOCATABLE,SAVE :: beta_save(:)  !< \f$\beta\f$ saved for restart
   !
 #if defined(_KOMEGA_THREAD)
   !$OMP THREADPRIVATE(z_seed, rho, alpha, alpha_old, beta, z, pi, &
@@ -83,9 +82,9 @@ MODULE komega_vecs_r
   IMPLICIT NONE
   !
   REAL(8),ALLOCATABLE,SAVE :: &
-  & v3(:), &
-  & p(:,:), &
-  & r_l_save(:,:)
+  & v3(:),        &
+  & p(:,:),       &
+  & r_l_save(:,:) &
   !
 #if defined(_KOMEGA_THREAD)
   !$OMP THREADPRIVATE(v3, p, r_l_save)
